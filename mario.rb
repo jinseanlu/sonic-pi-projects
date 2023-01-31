7c3b92cfdc8493f4559802bf2359eccded9fda68
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2023-01-31 16:31:11 -0500

    Create mario.rb

---
+++ mario.rb
@@ -0,0 +1,52 @@
+#This is sm1's music, have fun listening
+
+play :E
+sleep 0.2
+play :E
+sleep 0.35
+play :E
+sleep 0.5
+play :C
+sleep 0.2
+play :E
+sleep 0.2
+play :G4
+sleep 0.8
+play :G3
+sleep 0.7
+live_loop :cheeseburger do
+  play :C
+  sleep 0.5
+  play :G3
+  sleep 0.5
+  play :E3
+  sleep 0.7
+  play :A3
+  sleep 0.35
+  play :B3
+  sleep 0.35
+  play :A3
+  sleep 0.2
+  play :G3
+  sleep 0.5
+  play :G3
+  sleep 0.3
+  play :C
+  sleep 0.2
+  play :E
+  sleep 0.2
+  play :A
+  sleep 0.3
+  play :E
+  sleep 0.2
+  play :G
+  sleep 0.3
+  play :E
+  sleep 0.3
+  play :C
+  sleep 0.2
+  play :D
+  sleep 0.2
+  play :B3
+  sleep 0.5
+end
