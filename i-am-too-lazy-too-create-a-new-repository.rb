8ff14d149ba59a1d478ef33a3a40b06694d0dd32
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2023-03-01 15:37:57 -0500

    Create i-am-too-lazy-too-create-a-new-repository.rb

---
+++ i-am-too-lazy-too-create-a-new-repository.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+#The rand(value..value) means that it has a random value between the first and second number
+
+boolean = rand(1..2)
+if boolean == 1
+  puts "u are smart"
+elsif boolean == 2
+  puts "you have ok iq"
+end
+#This is an example of a string. the + is simple logic
+string = "The creator of this is JinSean"
+puts string +" and he is cool"
+
+#to be continued...
