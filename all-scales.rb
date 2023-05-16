610e56fe3d5705fb4981545fa96d56613aec0be2
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2023-05-16 15:50:44 -0400

    Create all-scales.rb

---
+++ all-scales.rb
@@ -0,0 +1,34 @@
+live_loop :cscale do; tick
+  play (scale :c, :major).look
+  sleep 0.5
+end
+sleep 0.08333333333333333333
+live_loop :dscale do; tick
+  play (scale :d, :major).look
+  sleep 0.5
+end
+sleep 0.08333333333333333333
+live_loop :escale do; tick
+  play (scale :e, :major).look
+  sleep 0.5
+end
+sleep 0.08333333333333333333
+live_loop :fscale do; tick
+  play (scale :f, :major).look
+  sleep 0.5
+end
+sleep 0.08333333333333333333
+live_loop :gscale do; tick
+  play (scale :g, :major).look
+  sleep 0.5
+end
+sleep 0.08333333333333333333
+live_loop :ascale do; tick
+  play (scale :a, :major).look
+  sleep 0.5
+end
+sleep 0.08333333333333333333
+live_loop :bscale do; tick
+  play (scale :b, :major).look
+  sleep 0.5
+end
