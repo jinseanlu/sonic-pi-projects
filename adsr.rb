f2a437a5a92666a4acd37bd116f8765e39b36726
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2023-06-06 15:17:16 -0400

    Update and rename pixelstuff.rb to adsr.rb

--- adsr.rb
+++ adsr.rb
@@ -1,11 +1,11 @@
 use_bpm 100
 live_loop :hi do
-  a = rand(1)
-  d = rand(2)
+  a = rand(2)
+  d = rand(3)
   s = rand(1)
   r = rand(2)
   sl = rand(1)
-  use_synth :chipnoise
+  use_synth :beep
   r = rand(1...2)
   if r = 1
     play :D4, attack: a, decay: d, sustain: s, release: r
